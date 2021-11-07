cf2d359754a6846a6f7c7eb737421ce8149f7492
Jonathan Felix <jhow.wi56@gmail.com>
2021-11-07 00:44:33 -0300

    finalizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import "./App.css"
-import DataTable from 'components/DataTable/dataTable';
-import BarChart from 'components/BarChart/barChart';
 
-import NavBar from 'components/NavBar';
-import Footer from 'components/Footer/footer';
-import DonutChart from 'components/DonutChart/donutChart';
 import Routes from 'Routes';
+import "./App.css";
+
 
 
 function App() {
